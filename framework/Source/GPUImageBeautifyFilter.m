ab39b1d8116f04fab3793a89006f98adfdaaa659
Nikolay Abkairov <nikolay.abkairov@gmail.com>
2017-03-22 16:47:18 -0700

    _adding CANNY define switch

--- framework/Source/GPUImageBeautifyFilter.m
+++ framework/Source/GPUImageBeautifyFilter.m
@@ -13,7 +13,10 @@
 #import "GPUImageThreeInputFilter.h"
 
 // Internal CombinationFilter(It should not be used outside)
-@interface GPUImageCombinationFilter : GPUImageTwoInputFilter // GPUImageThreeInputFilter
+#define USE_CANNY 0
+
+#if USE_CANNY
+@interface GPUImageCombinationFilter : GPUImageThreeInputFilter
 {
     GLint smoothDegreeUniform;
 }
@@ -29,25 +32,23 @@
 NSString *const kGPUImageBeautifyFragmentShaderString = NSStringMultiline (
  varying highp vec2 textureCoordinate;
  varying highp vec2 textureCoordinate2;
- //varying highp vec2 textureCoordinate3;
+ varying highp vec2 textureCoordinate3;
  
  uniform sampler2D inputImageTexture;
  uniform sampler2D inputImageTexture2;
- //uniform sampler2D inputImageTexture3;
+ uniform sampler2D inputImageTexture3;
  uniform mediump float smoothDegree;
  
  void main()
  {
      highp vec4 bilateral = texture2D(inputImageTexture, textureCoordinate);
-     //highp vec4 canny = texture2D(inputImageTexture2, textureCoordinate2);
-     //highp vec4 origin = texture2D(inputImageTexture3,textureCoordinate3);
-     highp vec4 origin = texture2D(inputImageTexture2,textureCoordinate2);
+     highp vec4 canny = texture2D(inputImageTexture2, textureCoordinate2);
+     highp vec4 origin = texture2D(inputImageTexture3,textureCoordinate3);
      highp vec4 smooth;
      lowp float r = origin.r;
      lowp float g = origin.g;
      lowp float b = origin.b;
-     //if (canny.r < 0.2 && r > 0.3725 && g > 0.1568 && b > 0.0784 && r > b && (max(max(r, g), b) - min(min(r, g), b)) > 0.0588 && abs(r-g) > 0.0588) {
-     if (r > 0.3725 && g > 0.1568 && b > 0.0784 && r > b && (max(max(r, g), b) - min(min(r, g), b)) > 0.0588 && abs(r-g) > 0.0588) {
+     if (canny.r < 0.2 && r > 0.3725 && g > 0.1568 && b > 0.0784 && r > b && (max(max(r, g), b) - min(min(r, g), b)) > 0.0588 && abs(r-g) > 0.0588) {
          smooth = (1.0 - smoothDegree) * (origin - bilateral) + bilateral;
      }
      else {
@@ -59,6 +60,58 @@ NSString *const kGPUImageBeautifyFragmentShaderString = NSStringMultiline (
      gl_FragColor = smooth;
  }
  );
+
+#else  // CANNY
+
+//NO CANNY
+
+@interface GPUImageCombinationFilter : GPUImageTwoInputFilter // GPUImageThreeInputFilter
+{
+    GLint smoothDegreeUniform;
+}
+
+@property (nonatomic, assign) CGFloat intensity;
+
+@end
+
+// N.B. Using GPUImage's SHADER_STRING causes macro replacement, which replaces 'log' with
+// something like 'tg_log' which leads to a crash.
+#define NSStringMultiline(text) @ #text
+
+NSString *const kGPUImageBeautifyFragmentShaderString = NSStringMultiline (
+    varying highp vec2 textureCoordinate;
+    varying highp vec2 textureCoordinate2;
+    //varying highp vec2 textureCoordinate3;
+
+    uniform sampler2D inputImageTexture;
+    uniform sampler2D inputImageTexture2;
+    //uniform sampler2D inputImageTexture3;
+    uniform mediump float smoothDegree;
+
+    void main()
+    {
+       highp vec4 bilateral = texture2D(inputImageTexture, textureCoordinate);
+       //highp vec4 canny = texture2D(inputImageTexture2, textureCoordinate2);
+       //highp vec4 origin = texture2D(inputImageTexture3,textureCoordinate3);
+       highp vec4 origin = texture2D(inputImageTexture2,textureCoordinate2);
+       highp vec4 smooth;
+       lowp float r = origin.r;
+       lowp float g = origin.g;
+       lowp float b = origin.b;
+       //if (canny.r < 0.2 && r > 0.3725 && g > 0.1568 && b > 0.0784 && r > b && (max(max(r, g), b) - min(min(r, g), b)) > 0.0588 && abs(r-g) > 0.0588) {
+       if (r > 0.3725 && g > 0.1568 && b > 0.0784 && r > b && (max(max(r, g), b) - min(min(r, g), b)) > 0.0588 && abs(r-g) > 0.0588) {
+           smooth = (1.0 - smoothDegree) * (origin - bilateral) + bilateral;
+       }
+       else {
+           smooth = origin;
+       }
+       smooth.r = log(1.0 + 0.2 * smooth.r)/log(1.2);
+       smooth.g = log(1.0 + 0.2 * smooth.g)/log(1.2);
+       smooth.b = log(1.0 + 0.2 * smooth.b)/log(1.2);
+       gl_FragColor = smooth;
+    }
+    );
+#endif //CANNY
 
 @implementation GPUImageCombinationFilter
 
@@ -104,9 +157,11 @@ NSString *const kGPUImageBeautifyFragmentShaderString = NSStringMultiline (
     bilateralFilter.distanceNormalizationFactor = 4.0;
     [self addFilter:bilateralFilter];
     
+#if USE_CANNY
     // Second pass: edge detection
-//    cannyEdgeFilter = [[GPUImageCannyEdgeDetectionFilter alloc] init];
-//    [self addFilter:cannyEdgeFilter];
+    cannyEdgeFilter = [[GPUImageCannyEdgeDetectionFilter alloc] init];
+    [self addFilter:cannyEdgeFilter];
+#endif
     
     // Third pass: combination bilateral, edge detection and origin
     combinationFilter = [[GPUImageCombinationFilter alloc] init];
@@ -118,11 +173,17 @@ NSString *const kGPUImageBeautifyFragmentShaderString = NSStringMultiline (
     [hsbFilter adjustSaturation:1.1F]; // disabled
     
     [bilateralFilter addTarget:combinationFilter];
-    //[cannyEdgeFilter addTarget:combinationFilter];
+#if USE_CANNY
+    [cannyEdgeFilter addTarget:combinationFilter];
+#endif
     
     [combinationFilter addTarget:hsbFilter];
 
+#if USE_CANNY
+    self.initialFilters = [NSArray arrayWithObjects:bilateralFilter,cannyEdgeFilter,combinationFilter,nil];
+#else
     self.initialFilters = [NSArray arrayWithObjects:bilateralFilter,/*cannyEdgeFilter,*/combinationFilter,nil];
+#endif
     self.terminalFilter = hsbFilter;
 
     return self;
